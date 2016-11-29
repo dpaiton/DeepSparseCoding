2f3596bbda76697a35cab5cc30616362dcf2b423
Dylan Paiton <dpaiton@gmail.com>
2016-11-29 12:34:47 -0800

    adds karklin_lewicki model

--- models/model_picker.py
+++ models/model_picker.py
@@ -1,9 +1,12 @@
 from models.mlp import MLP as mlp
+from models.karklin_lewicki import karklin_lewicki
 
 def get_model(params, schedule):
   if params["model_type"] == "mlp":
     return mlp(params, schedule)
+  if params["model_type"] == "karklin_lewicki":
+    return karklin_lewicki(params, schedule)
 
 def list_models():
-  model_list = ["mlp"]
+  model_list = ["mlp", "karklin_lewicki"]
   return model_list
