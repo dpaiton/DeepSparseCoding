873f17ddf2284d670fb29211215d2e4c56fe8bd6
Dylan Paiton <dpaiton@gmail.com>
2016-12-14 14:48:03 -0800

    adds compute_loss func; bug fixes
    fixes code for building unrolled graph
    new code uses compute_loss func in a loop
    improves logging and plotting abilities

--- models/karklin_lewicki.py
+++ models/karklin_lewicki.py
@@ -1,6 +1,7 @@
 import numpy as np
 import logging
 import tensorflow as tf
+import utils.plot_functions as pf
 from models.base_model import Model 
 
 class karklin_lewicki(Model):
@@ -23,10 +24,9 @@ class karklin_lewicki(Model):
     num_u          [int] Number of layer 1 elements
     num_v          [int] Number of layer 2 elements
     num_steps      [int] Number of inference steps
-    u_step_size    [float] Multiplier for gradient descent inference process
-    v_step_size    [float] Multiplier for gradient descent inference process
   """
   def load_params(self, params):
+  #TODO: Add rectify_u and rectify_v
     Model.load_params(self, params)
     # Meta parameters
     self.rectify_u = bool(params["rectify_u"])
@@ -42,30 +42,51 @@ class karklin_lewicki(Model):
     self.b_shape = [self.num_u, self.num_v]
     # Hyper Parameters
     self.num_steps = int(params["num_steps"])
-    self.u_step_size = float(params["u_step_size"])
-    self.v_step_size = float(params["v_step_size"])
 
-  def compute_loss(self, input_data, u_state, v_state):
+  """
+  Returns total loss function for given input
+  Outputs:
+    total_loss [float32] loss from Karklin & Lewicki
+  Inputs:
+    input_data [] 
+    u_state []
+    v_state []
+    a []
+    b []
+  """
+  def compute_loss(self, input_data, u_state=None, v_state=None, a=None,
+    b=None):
+    with tf.variable_scope("layers", reuse=True) as scope:
+      if u_state is None:
+        u_state = tf.get_variable("u")
+      if v_state is None:
+        v_state = tf.get_variable("v")
     with tf.variable_scope("weights", reuse=True) as scope:
-      a = tf.get_variable(name="a")
-      b = tf.get_variable(name="b")
+      if a is None:
+        a = tf.get_variable(name="a")
+      if b is None:
+        b = tf.get_variable(name="b")
     x_ = tf.matmul(a, u_state)
     sigma = tf.exp(-tf.matmul(b, v_state))
     recon_loss = self.recon_mult * tf.reduce_mean(0.5 *
       tf.reduce_sum(tf.pow(tf.sub(input_data, x_), 2.0),
       reduction_indices=[0]))
     feedback_loss = tf.reduce_mean(tf.reduce_sum(tf.add(
-      tf.div(u_state, sigma), tf.log(sigma)),
+      tf.abs(tf.div(u_state, sigma)), tf.log(sigma)),
       reduction_indices=[0]))
     sparse_loss = self.sparse_mult * tf.reduce_mean(
       tf.reduce_sum(tf.abs(v_state), reduction_indices=[0]))
     total_loss = (recon_loss + feedback_loss + sparse_loss)
-    return total_loss
+    return (total_loss, recon_loss, feedback_loss, sparse_loss)
 
   """
   Build the nework described in:
     Y Karklin, MS Lewicki (2005) - A Hierarchical Bayesian Model for Learning
       Nonlinear Statistical Regularities in Nonstationary Natural Signals
+  Method for unrolling inference into the graph
+    # compute loss for current sate
+    # take gradient of loss wrt current state
+    # compute new state = current state + eta * gradient
   """
   def build_graph(self):
     self.graph = tf.Graph()
@@ -78,6 +99,10 @@ class karklin_lewicki(Model):
             shape=(), name="recon_mult")
           self.sparse_mult = tf.placeholder(tf.float32,
             shape=(), name="sparse_mult")
+          self.u_step_size = tf.placeholder(tf.float32,
+            shape=(), name="u_step_size")
+          self.v_step_size = tf.placeholder(tf.float32,
+            shape=(), name="v_step_size")
 
         with tf.name_scope("constants") as scope:
           self.u_zeros = tf.zeros(
@@ -121,53 +146,38 @@ class karklin_lewicki(Model):
 
         with tf.name_scope("loss") as scope:
           with tf.name_scope("unsupervised"):
-            self.recon_loss = self.recon_mult * tf.reduce_mean(0.5 *
-              tf.reduce_sum(tf.pow(tf.sub(self.x, self.x_), 2.0),
-              reduction_indices=[0]))
-            self.feedback_loss = tf.reduce_mean(tf.reduce_sum(tf.add(
-              tf.div(self.u, self.sigma), tf.log(self.sigma)),
-              reduction_indices=[0]))
-            self.sparse_loss = self.sparse_mult * tf.reduce_mean(
-              tf.reduce_sum(tf.abs(self.v), reduction_indices=[0]))
-          self.total_loss = (self.recon_loss + self.feedback_loss +
-            self.sparse_loss)
+            (self.total_loss,
+            self.recon_loss,
+            self.feedback_loss,
+            self.sparse_loss) = self.compute_loss(self.x)
 
         with tf.name_scope("inference") as scope:
           self.clear_u = tf.group(self.u.assign(self.u_zeros))
           self.clear_v = tf.group(self.v.assign(self.v_zeros))
-          self.current_loss = []
+          current_loss = []
           self.u_t = [self.u_zeros]
           self.v_t = [self.v_zeros]
           for step in range(self.num_steps):
-            # compute loss for current sate
-            # take gradient of loss wrt current state
-            # compute new state = current state + eta * gradient
-            self.current_loss.append(self.compute_loss(self.x, self.u_t[step], self.v_t[step]))
-            self.du = -tf.gradients(self.current_loss[step], self.u_t[step])[0]
-            self.dv = -tf.gradients(self.current_loss[step], self.v_t[step])[0]
-            self.u_t.append(self.u_t[step] + self.u_step_size * self.du)
-            self.v_t.append(self.v_t[step] + self.v_step_size * self.dv)
-          if self.rectify_u:
-            new_u = tf.abs(self.u_t[-1])
-          else:
-            new_u = self.u_t[-1]
-          if self.rectify_v:
-            new_v = tf.abs(self.v_t[-1])
-          else:
-            new_v = self.v_t[-1]
-          self.do_inference = tf.group(self.u.assign(new_u),
-            self.v.assign(new_v), name="do_inference")
+            current_loss.append(self.compute_loss(self.x, self.u_t[step],
+              self.v_t[step])[0])
+            du = -tf.gradients(current_loss[step], self.u_t[step])[0]
+            dv = -tf.gradients(current_loss[step], self.v_t[step])[0]
+            self.u_t.append(self.u_t[step] + self.u_step_size * du)
+            self.v_t.append(self.v_t[step] + self.v_step_size * dv)
+          self.do_inference = tf.group(self.u.assign(self.u_t[-1]),
+            self.v.assign(self.v_t[-1]), name="do_inference")
 
     self.graph_built = True
 
   """
   Log train progress information
   Inputs:
-    input_data: load_MNIST data object containing the current image batch
-    input_label: load_MNIST data object containing the current label batch
+    input_data: data object containing the current image batch
+    input_label: data object containing the current label batch
     batch_step: current batch number within the schedule
   """
   def print_update(self, input_data, input_label=None, batch_step=0):
+  # TODO: Why did I have to get defult session for u/v and not loss?
     current_step = self.global_step.eval()
     feed_dict = self.get_feed_dict(input_data, input_label)
     u_vals = tf.get_default_session().run(self.u, feed_dict)
@@ -175,6 +185,12 @@ class karklin_lewicki(Model):
     logging.info("Global batch index is %g"%(current_step))
     logging.info("Finished step %g out of %g for schedule %g"%(batch_step,
       self.get_sched("num_batches"), self.sched_idx))
+    logging.info("\trecon loss:\t\t%g"%(
+      self.recon_loss.eval(feed_dict)))
+    logging.info("\tfeedback loss:\t\t%g"%(
+      self.feedback_loss.eval(feed_dict)))
+    logging.info("\tsparse loss:\t\t%g"%(
+      self.sparse_loss.eval(feed_dict)))
     logging.info("\ttotal loss:\t\t%g"%(
       self.total_loss.eval(feed_dict)))
     logging.info("\tmax val of u:\t\t%g"%(u_vals.max()))
@@ -190,34 +206,34 @@ class karklin_lewicki(Model):
   Plot weights, reconstruction, and gradients
   Inputs: input_data and input_label used for the session
   """
-  def generate_plots(self, input_image, input_label):
-    feed_dict = self.get_feed_dict(input_image, input_label)
+  def generate_plots(self, input_data, input_label=None):
+    feed_dict = self.get_feed_dict(input_data, input_label)
     current_step = str(self.global_step.eval())
     pf.save_data_tiled(
-      self.w.eval().reshape(self.num_classes,
-      int(np.sqrt(self.num_neurons)), int(np.sqrt(self.num_neurons))),
-      normalize=True, title="Classification matrix at step number "
-      +current_step, save_filename=(self.disp_dir+"w_v"+self.version+"-"
+      tf.transpose(self.b).eval().reshape(self.num_v,
+      int(np.sqrt(self.num_u)), int(np.sqrt(self.num_u))),
+      normalize=True, title="Density weights matrix at step number "
+      +current_step, save_filename=(self.disp_dir+"b_v"+self.version+"-"
       +current_step.zfill(5)+".pdf"))
     pf.save_data_tiled(
-      tf.transpose(self.phi).eval().reshape(self.num_neurons,
+      tf.transpose(self.a).eval().reshape(self.num_u,
       int(np.sqrt(self.num_pixels)), int(np.sqrt(self.num_pixels))),
       normalize=True, title="Dictionary at step "+current_step,
-      save_filename=(self.disp_dir+"phi_v"+self.version+"-"
+      save_filename=(self.disp_dir+"a_v"+self.version+"-"
       +current_step.zfill(5)+".pdf"))
     for weight_grad_var in self.grads_and_vars[self.sched_idx]:
       grad = weight_grad_var[0][0].eval(feed_dict)
       shape = grad.shape
       name = weight_grad_var[0][1].name.split('/')[1].split(':')[0]
-      if name == "phi":
-        pf.save_data_tiled(grad.T.reshape(self.num_neurons,
+      if name == "a":
+        pf.save_data_tiled(grad.T.reshape(self.num_u,
           int(np.sqrt(self.num_pixels)), int(np.sqrt(self.num_pixels))),
-          normalize=True, title="Gradient for phi at step "+current_step,
-          save_filename=(self.disp_dir+"dphi_v"+self.version+"_"
+          normalize=True, title="Gradient for a at step "+current_step,
+          save_filename=(self.disp_dir+"da_v"+self.version+"_"
           +current_step.zfill(5)+".pdf"))
-      elif name == "w":
-        pf.save_data_tiled(grad.reshape(self.num_classes,
-          int(np.sqrt(self.num_neurons)), int(np.sqrt(self.num_neurons))),
-          normalize=True, title="Gradient for w at step "+current_step,
-          save_filename=(self.disp_dir+"dw_v"+self.version+"_"
+      elif name == "b":
+        pf.save_data_tiled(grad.reshape(self.num_v,
+          int(np.sqrt(self.num_u)), int(np.sqrt(self.num_u))),
+          normalize=True, title="Gradient for b at step "+current_step,
+          save_filename=(self.disp_dir+"db_v"+self.version+"_"
           +current_step.zfill(5)+".pdf"))
