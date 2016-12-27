3ec092c9b519bb2dcea8608f294e3ada9f0a81b0
Dylan Paiton <dpaiton@gmail.com>
2016-12-27 11:56:20 -0800

    adds data loading, prototype for analysis

--- run_analysis.py
+++ run_analysis.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use("Agg")
 
 import os
+import models.model_picker as mp
 #from analysis.base_analysis import Analyzer as base_analysis
 #from analysis.karklin_lewicki import karklin_lewicki as kl_analysis
 import utils.log_parser as log_parser
@@ -25,6 +26,22 @@ log_text = log_parser.load_file(log_file_loc)
 model_params = log_parser.read_params(log_text)
 model_schedule = log_parser.read_schedule(log_text)
 model_stats = log_parser.read_stats(log_text)
+
+np_rand_state = np.random.RandomState(model_params["rand_seed"])
+
+data = load_MNIST(model_params["data_dir"],
+  normalize_imgs=model_params["norm_images"],
+  rand_state=np_rand_state)
+
+model = mp.get_model(model_params, model_schedule)
+
+#_, u_t, v_t, = sess.run([model.do_inference,
+#  model.u_t, model.v_t,],
+#  feed_dict)
+#print(np.max(u_t))
+#print("\n")
+#print(np.max(v_t))
+#print("\n")
 
 import IPython; IPython.embed(); raise SystemExit
 
