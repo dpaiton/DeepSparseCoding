48efc19b6f7cff2725f44290da79ebd2fecfbf65
Sophia Sanborn <sophia.sanborn@gmail.com>
2017-02-20 10:19:44 -0800

    adds gen_hier_data

--- data/genData.py
+++ data/genData.py
@@ -1,5 +1,6 @@
 import numpy as np
 from data.dataset import Dataset
+from scipy.stats import norm
 
 class DistData(object): 
   def __init__(self,
@@ -14,6 +15,7 @@ def load_dist(kwargs):
   data = DistData(kwargs["num_pixels"], kwargs["num_examples"])
   dist_type = kwargs["dist_type"]
   rand_seed = kwargs["rand_seed"]
+
   if dist_type == "gaussian":
     d = np.array([[[np.random.normal(kwargs["a"], kwargs["b"]) 
       for k in range(data.num_edge_pixels)] for j in range(data.num_edge_pixels)] 
@@ -23,4 +25,33 @@ def load_dist(kwargs):
       for k in range(data.num_edge_pixels)] for j in range(data.num_edge_pixels)] 
       for i in range(data.num_examples)], dtype=np.float32)
   data = Dataset(d, None, None)
+  return {"train": data}
+
+"""
+Generate sparse hierarchical dataset
+"""
+def gen_hier_data(kwargs):
+  I = kwargs["num_pixels"]
+  N = kwargs["num_examples"]
+
+  # Number of second-level basis functions
+  J = 10 
+
+  # First-level identity basis
+  A = np.identity(I)
+
+  # Second-level Gaussian basis
+  sig = int(np.sqrt(I)/2)
+  B = np.asarray([norm.pdf(np.arange(0, I), np.random.randint(0, I), sig) 
+    for i in range(J)], dtype = np.float32).T
+  B /= (np.max(B, axis = 0)[None,:])
+
+  # Laplacian prior over B
+  v = np.random.laplace(0, 1, (J, N))
+  lam = np.exp(B @ v)
+  u = np.sqrt(lam) * np.random.normal(0, 1, (I, N))
+
+  # Generate data
+  x = np.reshape(A @ u, (N, int(np.sqrt(I)), int(np.sqrt(I))))
+  data = Dataset(x, None, None)
   return {"train": data}
