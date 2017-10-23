9221a948782ac31653d9c9caaf5ad3fe502c2a58
Dylan <dpaiton@gmail.com>
2017-10-23 15:27:37 -0700

    moves comments into function definitions

--- models/base_model.py
+++ models/base_model.py
@@ -19,12 +19,12 @@ class Model(object):
     self.graph = tf.Graph()
     self.setup_graph()
 
-  """
-  Load schedule into object
-  Inputs:
-   schedule: [list of dict] learning schedule
-  """
   def load_schedule(self, schedule):
+    """
+    Load schedule into object
+    Inputs:
+     schedule: [list of dict] learning schedule
+    """
     for sched in schedule:
       assert len(sched["weights"]) == len(sched["weight_lr"])
       assert len(sched["weights"]) == len(sched["decay_steps"])
@@ -32,37 +32,37 @@ class Model(object):
       assert len(sched["weights"]) == len(sched["staircase"])
     self.sched = schedule
 
-  """
-  Load parameters into object
-  Inputs:
-   params: [dict] model parameters
-  Modifiable Parameters:
-    model_type     [str] Type of model
-                     Can be "MLP", "karklin_lewicki"
-    model_name     [str] Name for model
-    out_dir        [str] Base directory where output will be directed
-    version        [str] Model version for output
-    optimizer      [str] Which optimization algorithm to use
-                     Can be "annealed_sgd" or "adam"
-    log_int        [int] How often to send updates to stdout
-    log_to_file    [bool] If set, log to file, else log to stderr
-    gen_plot_int   [int] How often to generate plots
-    save_plots     [bool] If set, save plots to file
-    cp_int         [int] How often to checkpoint
-    max_cp_to_keep [int] How many checkpoints to keep. See max_to_keep tf arg
-    cp_load        [bool] if set, load from checkpoint
-    cp_load_name   [str] Checkpoint model name to load
-    cp_load_step   [int] Checkpoint time step to load
-    cp_load_ver    [str] Checkpoint version to load
-    cp_load_var    [list of str] which variables to load
-                     if None or empty list, the model load all weights
-    cp_set_var     [list of str] which variables to assign values to
-                     len(cp_set_var) should equal len(cp_load_var)
-    eps            [float] Small value to avoid division by zero
-    device         [str] Which device to run on
-    rand_seed      [int] Random seed
-  """
   def load_params(self, params):
+    """
+    Load parameters into object
+    Inputs:
+     params: [dict] model parameters
+    Modifiable Parameters:
+      model_type     [str] Type of model
+                       Can be "MLP", "karklin_lewicki"
+      model_name     [str] Name for model
+      out_dir        [str] Base directory where output will be directed
+      version        [str] Model version for output
+      optimizer      [str] Which optimization algorithm to use
+                       Can be "annealed_sgd" or "adam"
+      log_int        [int] How often to send updates to stdout
+      log_to_file    [bool] If set, log to file, else log to stderr
+      gen_plot_int   [int] How often to generate plots
+      save_plots     [bool] If set, save plots to file
+      cp_int         [int] How often to checkpoint
+      max_cp_to_keep [int] How many checkpoints to keep. See max_to_keep tf arg
+      cp_load        [bool] if set, load from checkpoint
+      cp_load_name   [str] Checkpoint model name to load
+      cp_load_step   [int] Checkpoint time step to load
+      cp_load_ver    [str] Checkpoint version to load
+      cp_load_var    [list of str] which variables to load
+                       if None or empty list, the model load all weights
+      cp_set_var     [list of str] which variables to assign values to
+                       len(cp_set_var) should equal len(cp_load_var)
+      eps            [float] Small value to avoid division by zero
+      device         [str] Which device to run on
+      rand_seed      [int] Random seed
+    """
     self.params = params
     # Meta-parameters
     self.model_name = str(params["model_name"])
@@ -107,34 +107,34 @@ class Model(object):
     self.device = str(params["device"])
     self.rand_seed = int(params["rand_seed"])
 
-  """Check parameters with assertions"""
   def check_params(self):
+    """Check parameters with assertions"""
     pass
 
-  """
-  Get param value from model
-    This is equivalent to self.param_name, except that it will return None if
-    the param does not exist.
-  """
   def get_param(self, param_name):
+    """
+    Get param value from model
+      This is equivalent to self.param_name, except that it will return None if
+      the param does not exist.
+    """
     if hasattr(self, param_name):
       return getattr(self, param_name)
     else:
       return None
 
-  """
-  Modifies a model parameter
-  Inputs:
-    param_name: [str] parameter name, must already exist
-    new_value: [] new parameter value, must be the same type as old param value
-  """
   def set_param(self, param_name, new_value):
+    """
+    Modifies a model parameter
+    Inputs:
+      param_name: [str] parameter name, must already exist
+      new_value: [] new parameter value, must be the same type as old param value
+    """
     assert hasattr(self, param_name)
     assert type(getattr(self, param_name)) == type(new_value)
     setattr(self, param_name, new_value)
 
-  """Make output directories"""
   def make_dirs(self):
+    """Make output directories"""
     if not os.path.exists(self.out_dir):
       os.makedirs(self.out_dir)
     if not os.path.exists(self.log_dir):
@@ -146,8 +146,8 @@ class Model(object):
     if not os.path.exists(self.disp_dir):
       os.makedirs(self.disp_dir)
 
-  """Logging to track run statistics"""
   def init_logging(self):
+    """Logging to track run statistics"""
     logging_level = logging.INFO
     log_format = ("%(asctime)s.%(msecs)03d"
       +" -- %(message)s")
@@ -162,8 +162,8 @@ class Model(object):
       logging.basicConfig(format=log_format, datefmt=date_format,
         level=logging.INFO)
 
-  """Use logging to write model params"""
   def log_params(self, params=None):
+    """Use logging to write model params"""
     if params is not None:
       dump_obj = params
     else:
@@ -173,27 +173,27 @@ class Model(object):
     js_str = js.dumps(dump_obj, sort_keys=True, indent=2)
     logging.info("<params>"+js_str+"</params>")
 
-  """Use logging to write current schedule, as specified by self.sched_idx"""
   def log_schedule(self):
+    """Use logging to write current schedule, as specified by self.sched_idx"""
     js_str = js.dumps(self.sched, sort_keys=True, indent=2)
     logging.info("<schedule>"+js_str+"</schedule>")
 
-  """Log input string"""
   def log_info(self, string):
+    """Log input string"""
     logging.info(str(string))
 
-  """Returns the gradients for a weight variable using a given optimizer"""
   def compute_weight_gradients(self, optimizer, weight_op=None):
+    """Returns the gradients for a weight variable using a given optimizer"""
     return optimizer.compute_gradients(self.total_loss, var_list=weight_op)
 
-  """
-  Add optimizers to graph
-  Creates member variables grads_and_vars and apply_grads for each weight
-    - both member variables are indexed by [schedule_idx][weight_idx]
-    - grads_and_vars holds the gradients for the weight updates
-    - apply_grads is the operator to be called to perform weight updates
-  """
   def add_optimizers_to_graph(self):
+    """
+    Add optimizers to graph
+    Creates member variables grads_and_vars and apply_grads for each weight
+      - both member variables are indexed by [schedule_idx][weight_idx]
+      - grads_and_vars holds the gradients for the weight updates
+      - apply_grads is the operator to be called to perform weight updates
+    """
     with self.graph.as_default():
       with tf.name_scope("optimizers") as scope:
         self.grads_and_vars = list()
@@ -231,11 +231,11 @@ class Model(object):
           self.apply_grads.append(sch_apply_grads)
     self.optimizers_added = True
 
-  """
-  Add initializer to the graph
-  This must be done after optimizers have been added
-  """
   def add_initializer_to_graph(self):
+    """
+    Add initializer to the graph
+    This must be done after optimizers have been added
+    """
     assert self.optimizers_added
     with tf.device(self.device):
       with self.graph.as_default():
@@ -243,8 +243,8 @@ class Model(object):
           self.init_op = tf.group(tf.global_variables_initializer(),
             tf.local_variables_initializer())
 
-  """Get variables for loading"""
   def get_load_vars(self):
+    """Get variables for loading"""
     all_vars = tf.global_variables()
     if len(self.cp_load_var) > 0:
       load_v = [var
@@ -256,8 +256,8 @@ class Model(object):
       load_v = all_vars
     return load_v
 
-  """Add savers to graph"""
   def construct_savers(self):
+    """Add savers to graph"""
     assert self.optimizers_added, (
       "Optimizers must be added to the graph before constructing savers.")
     with self.graph.as_default():
@@ -271,8 +271,8 @@ class Model(object):
         self.loader = tf.train.Saver(var_list=self.get_load_vars())
     self.savers_constructed = True
 
-  """Write saver definitions for full model and weights-only"""
   def write_saver_defs(self):
+    """Write saver definitions for full model and weights-only"""
     assert self.savers_constructed
     full_saver_def = self.full_saver.as_saver_def()
     full_file = self.save_dir+self.model_name+"_v"+self.version+"-full.def"
@@ -285,16 +285,16 @@ class Model(object):
       f.write(weight_saver_def.SerializeToString())
     logging.info("Weight saver def saved in file %s"%weight_file)
 
-  """Write graph structure to protobuf file"""
   def write_graph(self, graph_def):
+    """Write graph structure to protobuf file"""
     write_name = self.model_name+"_v"+self.version+".pb"
     self.writer = tf.summary.FileWriter(self.save_dir, graph=self.graph)
     tf.train.write_graph(graph_def,
       logdir=self.save_dir, name=write_name, as_text=False)
     logging.info("Graph def saved in file %s"%self.save_dir+write_name)
 
-  """Write checkpoints for full model and weights-only"""
   def write_checkpoint(self, session):
+    """Write checkpoints for full model and weights-only"""
     base_save_path = self.cp_save_dir+self.model_name+"_v"+self.version
     full_save_path = self.full_saver.save(session,
       save_path=base_save_path+"_full",
@@ -306,56 +306,56 @@ class Model(object):
     logging.info("Weights model saved in file %s"%weight_save_path)
     return base_save_path
 
-  """
-  Returns the current schedule being executed
-  Inputs:
-    key: [str] key in dictionary
-  """
   def get_sched(self, key=None):
+    """
+    Returns the current schedule being executed
+    Inputs:
+      key: [str] key in dictionary
+    """
     if key is not None:
       assert key in self.sched[self.sched_idx].keys(), (
         key+" was not found in the schedule.")
       return self.sched[self.sched_idx][key]
     return self.sched[self.sched_idx]
 
-  """
-  Modifies the internal schedule for the current schedule index
-  Inputs:
-    key: [str] key in dictionary
-    val: value be set in schedlue,
-      if there is already a val for key, new val must be of same type
-  """
   def set_sched(self, key, val):
+    """
+    Modifies the internal schedule for the current schedule index
+    Inputs:
+      key: [str] key in dictionary
+      val: value be set in schedlue,
+        if there is already a val for key, new val must be of same type
+    """
     if key in self.sched[self.sched_idx].keys():
       assert type(val) == type(self.sched[self.sched_idx][key]), (
         "val must have type "+str(type(self.sched[self.sched_idx][key])))
     self.sched[self.sched_idx][key] = val
 
-  """
-  Load checkpoint model into session.
-  Inputs:
-    session: tf.Session() that you want to load into
-    model_dir: String specifying the path to the checkpoint
-  """
   def load_full_model(self, session, model_dir):
+    """
+    Load checkpoint model into session.
+    Inputs:
+      session: tf.Session() that you want to load into
+      model_dir: String specifying the path to the checkpoint
+    """
     self.full_saver.restore(session, model_dir)
 
-  """
-  Load checkpoint weights into session.
-  Inputs:
-    session: tf.Session() that you want to load into
-    model_dir: String specifying the path to the checkpoint
-  """
   def load_weights(self, session, model_dir):
+    """
+    Load checkpoint weights into session.
+    Inputs:
+      session: tf.Session() that you want to load into
+      model_dir: String specifying the path to the checkpoint
+    """
     self.weight_saver.restore(session, model_dir)
 
-  """
-  Return dictionary containing all placeholders
-  Inputs:
-    input_data: data to be placed in self.x
-    input_labels: label to be placed in self.y
-  """
   def get_feed_dict(self, input_data, input_labels=None):
+    """
+    Return dictionary containing all placeholders
+    Inputs:
+      input_data: data to be placed in self.x
+      input_labels: label to be placed in self.y
+    """
     placeholders = [op.name
       for op
       in self.graph.get_operations()
@@ -371,36 +371,36 @@ class Model(object):
         self.get_sched(placeholder.split("/")[1]))
     return feed_dict
 
-  """Setup graph object and add optimizers, initializer"""
   def setup_graph(self):
+    """Setup graph object and add optimizers, initializer"""
     self.build_graph()
     self.add_optimizers_to_graph()
     self.add_initializer_to_graph()
     self.construct_savers()
 
-  """Build the TensorFlow graph object"""
   def build_graph(self):
+    """Build the TensorFlow graph object"""
     pass
 
-  """
-  Log train progress information
-  Inputs:
-    input_data: data object containing the current image batch
-    input_labels: data object containing the current label batch
-    batch_step: current batch number within the schedule
-  NOTE: For the analysis code to parse update statistics, the js.dumps() call
-    must receive a dict object. Additionally, the js.dumps() output must be
-    logged with <stats> </stats> tags.
-    For example: logging.info("<stats>"+js.dumps(output_dictionary)+"</stats>")
-  """
   def print_update(self, input_data, input_labels=None, batch_step=0):
+    """
+    Log train progress information
+    Inputs:
+      input_data: data object containing the current image batch
+      input_labels: data object containing the current label batch
+      batch_step: current batch number within the schedule
+    NOTE: For the analysis code to parse update statistics, the js.dumps() call
+      must receive a dict object. Additionally, the js.dumps() output must be
+      logged with <stats> </stats> tags.
+      For example: logging.info("<stats>"+js.dumps(output_dictionary)+"</stats>")
+    """
     pass
 
-  """
-  Plot weights, reconstruction, gradients, etc
-  Inputs:
-    input_data: data object containing the current image batch
-    input_labels: data object containing the current label batch
-  """
   def generate_plots(self, input_data, input_labels=None):
+    """
+    Plot weights, reconstruction, gradients, etc
+    Inputs:
+      input_data: data object containing the current image batch
+      input_labels: data object containing the current label batch
+    """
     pass
