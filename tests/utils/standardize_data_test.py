d5607742c5507a91c191ca24055833ef8715d90b
Dylan Paiton <dpaiton@users.noreply.github.com>
2017-10-30 16:38:46 -0700

    test for standardize_data function in utils/image_processing (#14)

---
+++ tests/utils/standardize_data_test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+import utils.image_processing as ip
+"""
+Test for the standardize_data function.
+NOTE: Should be executed from the repository's root directory
+"""
+class StandardizeDataTest(tf.test.TestCase):
+  def testBasic(self):
+    # TODO: err should scale with variance
+    err = 1e-15; rand_mean=5.0; rand_var=1000.0;
+    num_samples=10; num_rows=16; num_cols=16
+    rand_state = np.random.RandomState(1234)
+    gpu_args = [True, False] if tf.test.is_gpu_available(cuda_only=True) else [False]
+    for use_gpu in gpu_args:
+      with self.test_session(use_gpu=use_gpu):
+        data = rand_state.normal(rand_mean, rand_var, size=[num_samples, num_rows, num_cols])
+        stand_data = ip.standardize_data(data)
+        for idx in range(stand_data.shape[0]):
+          self.assertNear(np.mean(stand_data[idx, ...]), 0.0, err)
+          self.assertNear(np.std(data[idx, ...]), 1.0, err)
+
+if __name__ == "__main__":
+  tf.test.main()
