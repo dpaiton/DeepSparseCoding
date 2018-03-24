bd7b5a0597f3b5722ff78e39e269976a14581200
Dylan <dpaiton@gmail.com>
2018-03-24 13:29:59 -0700

    not maintaining density analysis

--- analysis/analysis_picker.py
+++ analysis/analysis_picker.py
@@ -1,10 +1,8 @@
-from analysis.ica_analyzer import ICA as ica_analyzer
-from analysis.lca_analyzer import LCA as lca_analyzer
-from analysis.lca_pca_analyzer import LCA_PCA as lca_pca_analyzer
-from analysis.lca_pca_fb_analyzer import LCA_PCA_FB as lca_pca_fb_analyzer
-from analysis.conv_lca_analyzer import CONV_LCA as conv_lca_analyzer
-from analysis.dsc_analyzer import DSC as dsc_analyzer
-from analysis.density_analyzer import density_analyzer as density_analyzer
+from analysis.ica_analyzer import ICA_Analyzer as ica_analyzer
+from analysis.lca_analyzer import LCA_Analyzer as lca_analyzer
+from analysis.lca_pca_analyzer import LCA_PCA_Analyzer as lca_pca_analyzer
+from analysis.lca_pca_fb_analyzer import LCA_PCA_FB_Analyzer as lca_pca_fb_analyzer
+from analysis.conv_lca_analyzer import CONV_LCA_Analyzer as conv_lca_analyzer
 
 def get_analyzer(params):
   if params["model_type"].lower() == "ica":
@@ -17,8 +15,4 @@ def get_analyzer(params):
     return lca_pca_fb_analyzer(params)
   if params["model_type"].lower() == "conv_lca":
     return conv_lca_analyzer(params)
-  if params["model_type"].lower() == "DSC":
-    return dsc_analyzer(params)
-  if params["model_type"].lower() == "density_learner":
-    return density_analyzer(params)
   assert False, ("model_type did not match allowable types in analysis.get_analyzer.")
