9c86b660a16470d8aec1f27b21240437ff24ba3c
rzarcone <ryanzarcone@gmail.com>
2018-06-28 09:51:27 -0700

    Adds gdn decoder; new plots; new architecture
    adds conv gdn decoder
    adds new plots to montior train
    new conv architecture

--- models/model_picker.py
+++ models/model_picker.py
@@ -12,6 +12,7 @@ from models.conv_lca import CONV_LCA as conv_lca
 from models.sigmoid_autoencoder import Sigmoid_Autoencoder as sa
 from models.gdn_autoencoder import GDN_Autoencoder as ga
 from models.conv_gdn_autoencoder import Conv_GDN_Autoencoder as cga
+from models.conv_gdn_decoder import Conv_GDN_Decoder as cgd
 from models.relu_autoencoder import ReLU_Autoencoder as ra
 from models.density_learner import Density_Learner as dl
 
@@ -42,6 +43,8 @@ def get_model(model_type):
     return ga()
   if model_type.lower() == "conv_gdn_autoencoder":
     return cga()
+  if model_type.lower() == "conv_gdn_decoder":
+    return cgd()
   if model_type.lower() == "relu_autoencoder":
     return ra()
   if model_type.lower() == "density_learner":
@@ -49,6 +52,6 @@ def get_model(model_type):
 
 def get_model_list():
   model_list = ["mlp", "ica", "ica_pca", "rica", "gradient_sc", "entropy_sc", "lca", "lca_pca",
-    "lca_pca_fb", "conv_lca", "sigmoid_autoencoder", "gdn_autoencoder", "conv_gdn_autoencoder",
+    "lca_pca_fb", "conv_lca", "sigmoid_autoencoder", "gdn_autoencoder", "conv_gdn_autoencoder", "conv_gdn_decoder"
     "relu_autoencoder", "density_learner"]
   return model_list
