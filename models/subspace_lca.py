0293432e2c97678af184ee412df92913115bdb95
Dylan <dpaiton@gmail.com>
2018-10-12 17:19:22 -0700

    rewrote subspace definitions, runs about twice as fast now

--- models/subspace_lca.py
+++ models/subspace_lca.py
@@ -18,64 +18,56 @@ class SUBSPACE_LCA(LCA):
     assert self.num_neurons % self.num_groups == 0, (
       "Num groups must divide evenly into num neurons.")
     self.num_neurons_per_group = int(self.num_neurons / self.num_groups)
-    neuron_ids = np.arange(self.num_neurons, dtype=np.int32)
-    self.rand_state.shuffle(neuron_ids) #happens in-place
     # group_ids is a list of len == self.num_groups, where each element in the list is a list of
     #    neuron indices assigned to that group
+    neuron_ids = np.arange(self.num_neurons, dtype=np.int32)
     self.group_ids = [neuron_ids[start:start+self.num_neurons_per_group]
-      for start in range(0, self.num_neurons, self.num_neurons_per_group)]
+      for start in np.arange(0, self.num_neurons, self.num_neurons_per_group, dtype=np.int32)]
     # group_assignemnts is an array of shape [self.num_neurons] that conatins a group assignment
     #    for each neuron index
-    self.group_assignments = np.zeros(self.num_neurons, dtype=np.float32)
+    self.group_assignments = np.zeros(self.num_neurons, dtype=np.int32)
     for group_index, group_member_indices in enumerate(self.group_ids):
       for neuron_id in group_member_indices:
         self.group_assignments[neuron_id] = group_index
 
-  def reshape_groups_per_neuron(self, group_amplitudes, name=None):
+  def reshape_groups_per_neuron(self, sigmas, name=None):
     """
     Reshape sigmas from [num_batch, num_groups] to [num_batch, num_neurons]
     Each neuron index is assigned the group amplitude for its corresponding group
     """
-    sigmas_resh = tf.stack([group_amplitudes[:, int(group_index)]
-      for group_index in self.group_assignments], axis=-1, name=name)
-    return sigmas_resh
+    sigmas = tf.squeeze(tf.stack([sigmas[:, group_index]
+      for group_index in self.group_assignments], axis=-1, name=name))
+    return sigmas
 
   def group_amplitudes(self, a_in, name=None):
     """
     group_amplitudes returns each neuron's group index:
       sigma_i = ||a_{j in i}||_2
-    TODO: This comment and the ones inline are confusing...
-    Although sigmas should be the shape [num_batch, num_group], here we reshape it so that
-    it can be easily used for thresholding later.
+    a_in shape is [num_batch, num_neurons]
+    sigmas shape is [num_batch, num_groups]
     """
-    # [num_batch, num_neurons_per_group, num_groups]
-    a_column_slices = tf.stack([self.slice_features(a_in, group_member_indices)
-      for group_member_indices in self.group_ids], axis=-1)
-    # [num_batch, num_groups]
-    sigmas = tf.sqrt(tf.reduce_sum(tf.square(a_column_slices), axis=1, keep_dims=False))
+    new_shape = tf.stack([tf.shape(self.x)[0]]+[self.num_groups, self.num_neurons_per_group])
+    a_resh = tf.reshape(a_in, shape=new_shape)
+    sigmas = tf.sqrt(tf.reduce_sum(tf.square(a_resh), axis=2, keep_dims=True), name=name)
     return sigmas
 
   def group_directions(self, a_in, sigmas, name=None):
     """
     group_directions returns each neurons direction normalized by the group amplitude:
       \hat{a}_i = a_i / sigmas_i
-    sigmas should be the same shape as a_i,
-    which is obtained using self.reshape_groups_per_neuron
+    a_in shape [num_batch, num_neurons]
+    sigms shape [num_batch, num_neurons]
+    directions shape [num_batch, num_neurons]
     """
     directions = tf.divide(a_in, sigmas, name=name)
     return directions
-
-  def get_group_weights(self):
-    weights = [self.slice_features(self.phi, group_member_indices)
-      for group_member_indices in self.group_ids]
-    return weights
 
   def threshold_units(self, u_in):
     """
     All units in a group are above threshold if their group amplitude is above lambda
     """
-    u_amplitudes = self.reshape_groups_per_neuron(self.group_amplitudes(u_in), name="u_amplitudes")
-    u_directions = self.group_directions(u_in, u_amplitudes, name="u_directions")
+    u_amplitudes = self.reshape_groups_per_neuron(self.group_amplitudes(u_in)) # [num_batch, num_neurons]
+    u_directions = self.group_directions(u_in, u_amplitudes) # [num_batch, num_neurons]
     a_out = tf.where(tf.greater(u_amplitudes, self.sparse_mult),
       tf.multiply(tf.subtract(u_amplitudes, self.sparse_mult), u_directions),
       self.u_zeros)
@@ -94,9 +86,11 @@ class SUBSPACE_LCA(LCA):
         # assemble matrix of W = [num_pixels, num_neurons_in_group]
         # compute E =  ( W^T * W ) - I
         # loss = mean(loss_mult * E)
-      w_slices = self.get_group_weights()
-      w_orth_list = [tf.reduce_sum(tf.abs(tf.subtract(tf.matmul(tf.transpose(w_slices[group_idx]),
-        w_slices[group_idx]), tf.eye(self.num_neurons_per_group, self.num_neurons_per_group))))
+      group_weights = tf.reshape(self.phi,
+        shape=[self.num_pixels, self.num_groups, self.num_neurons_per_group], name="group_weights")
+      w_orth_list = [
+        tf.reduce_sum(tf.abs(tf.subtract(tf.matmul(tf.transpose(group_weights[:,group_idx,:]),
+        group_weights[:,group_idx,:]), tf.eye(num_rows=self.num_neurons_per_group))))
         for group_idx in range(self.num_groups)]
       group_orthogonalization_loss = tf.multiply(self.group_orth_mult, tf.add_n(w_orth_list),
         name="group_orth_loss")
@@ -115,13 +109,10 @@ class SUBSPACE_LCA(LCA):
     super(SUBSPACE_LCA, self).build_graph()
     with tf.device(self.device):
       with self.graph.as_default():
-        with tf.variable_scope(self.weight_scope):
-          self.group_weights = tf.stack(self.get_group_weights(), axis=1, name="group_weights")
         with tf.variable_scope("inference"):
           self.group_activity = tf.identity(self.group_amplitudes(self.a), name="group_activity")
-  #      with tf.name_scope("norm_groups") as scope:
-  #        self.norm_group = # orthogonalize weights within group
-  #        self.norm_groups = tf.group(self.norm_phi, name="group_orthogonlization")
+          self.group_angles = tf.identity(self.group_directions(self.a, self.group_activity),
+            name="group_directions")
 
   def generate_plots(self, input_data, input_labels=None):
     """
