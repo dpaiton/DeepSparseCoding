4a7e1b58492e8358458433ef647dac6c1fb415cf
Dylan <dpaiton@gmail.com>
2018-10-29 10:16:42 -0700

    new params for mnist & experimental conv gdn

--- params/conv_gdn_autoencoder_params.py
+++ params/conv_gdn_autoencoder_params.py
@@ -4,19 +4,20 @@ import numpy as np
 params = {
   "model_type": "conv_gdn_autoencoder",
   "model_name": "conv_gdn_autoencoder_pretrain_exp",
-  "version": "4.0",
+  "version": "0.0",
   "vectorize_data": False,
   "norm_data": False,
   "center_data": False,
   "standardize_data": False,
   "contrast_normalize": False,
-  "whiten_data": False,
+  "whiten_data": True,
+  "whiten_method": "FT",
   "lpf_data": True,
   "lpf_cutoff": 0.7,
   "extract_patches": False,
   "im_size_y": 64,
   "im_size_x": 64,
-  "batch_size": 25,
+  "batch_size": 50,
   "num_colors": 1,
   "downsample_images": True,
   "downsample_method": "resize",
@@ -35,7 +36,7 @@ params = {
   "strides": [4, 2, 2],
   "w_thresh_min": 1e-3,
   "b_thresh_min": 1e-3,
-  "gdn_mult_min": 1e-6,
+  "gdn_mult_min": 1e-5,
   "memristor_type": "rram",
   "memristor_data_loc": os.path.expanduser("~")+"/CAE_Project/CAEs/data/Partial_Reset_PCM.pkl",
   "optimizer": "annealed_sgd",
@@ -47,7 +48,7 @@ params = {
   "cp_load_ver": "1.0",
   "log_int": 100,
   "log_to_file": True,
-  "gen_plot_int": 1000,
+  "gen_plot_int": 10000,
   "save_plots": True,
   "eps": 1e-12,
   "device": "/gpu:0",
@@ -71,20 +72,20 @@ gdn_list = w_gdn_list + b_gdn_list + w_igdn_list + b_igdn_list
 train_list = conv_list +  gdn_list
 params["cp_load_var"] = train_list
 
-weight_lr = [1.0e-3 for _ in range(len(conv_list))]
-weight_lr += [6.0e-4 for _ in range(len(gdn_list))]
+weight_lr = [3.0e-4 for _ in range(len(conv_list))]
+weight_lr += [1.0e-4 for _ in range(len(gdn_list))]
 decay_rate = [0.8 for _ in range(len(train_list))]
 staircase = [True for _ in range(len(train_list))]
 
-schedule = [ # TODO SCHEDULE
+schedule = [
   {"weights": train_list,
   "ent_mult": 0.1,
   "ramp_slope": 0.8,
-  "decay_mult": 0.001,
+  "decay_mult": 0.002,
   "noise_var_mult": 0.0,
   "mem_error_rate": 0.0,
   "triangle_centers": np.linspace(-1.0, 1.0, params["num_triangles"]),
   "weight_lr": weight_lr,
-  "num_epochs": 10,
+  "num_epochs": 10, #10 takes about 13 hours on rw1 with batch size 50
   "decay_rate": decay_rate,
   "staircase": staircase}]
