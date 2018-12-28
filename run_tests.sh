9b5f16ab481c0fe4ca2da1f2d3f22d64715a2887
Sheng Lundquist <slundqui007@gmail.com>
2018-12-27 17:28:28 -0800

    run_tests.sh now takes a flag that tests training of models

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-#echo "----TEST 1 of 5"
-#CUDA_VISIBLE_DEVICES=0 python3 tests/utils/reshape_data_test.py
-#echo "----TEST 2 of 5"
-#CUDA_VISIBLE_DEVICES=0 python3 tests/utils/standardize_data_test.py
-#echo "----TEST 3 of 5"
-#CUDA_VISIBLE_DEVICES=0 python3 tests/utils/patches_test.py
-#echo "----TEST 4 of 5"
-#CUDA_VISIBLE_DEVICES=0 python3 tests/utils/contrast_normalize_test.py
-echo "----TEST 5 of 5"
+if [ "$1" = "--train" ]; then
+  NUM_TEST="6"
+else
+  NUM_TEST="5"
+fi
+
+echo "----TEST 1 of $NUM_TEST"
+CUDA_VISIBLE_DEVICES=0 python3 tests/utils/reshape_data_test.py
+echo "----TEST 2 of $NUM_TEST"
+CUDA_VISIBLE_DEVICES=0 python3 tests/utils/standardize_data_test.py
+echo "----TEST 3 of $NUM_TEST"
+CUDA_VISIBLE_DEVICES=0 python3 tests/utils/patches_test.py
+echo "----TEST 4 of $NUM_TEST"
+CUDA_VISIBLE_DEVICES=0 python3 tests/utils/contrast_normalize_test.py
+echo "----TEST 5 of $NUM_TEST"
 CUDA_VISIBLE_DEVICES=0 python3 tests/models/models_test.py
-#TODO: do this if flag is set
-#CUDA_VISIBLE_DEVICES=0 python3 tests/training/training_test.py
+if [ "$1" == "--train" ]; then
+  echo "----TEST 6 of $NUM_TEST"
+  CUDA_VISIBLE_DEVICES=0 python3 tests/training/training_test.py
+fi
