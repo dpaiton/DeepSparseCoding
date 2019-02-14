2ffab9b3d01bde2ef332140d6e54053d6ed92e65
ryanchankh <ryanchankh@berkeley.edu>
2019-02-14 04:02:36 -0800

    added ica subspace params file and added ica subspace to param picker

---
+++ params/ica_subspace_params.py
@@ -0,0 +1,11 @@
+import os 
+import numpyas np 
+
+class params(BaseParams):
+  def __init__(self):
+    super(params, self).__init__()
+    self.model_type = "ica_subspace"
+    self.model_name = "ica_subspace"
+    self.version = "0.0"
+
+
