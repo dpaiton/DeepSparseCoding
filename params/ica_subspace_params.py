be51c74cf07597bffd37797fadb4f3f703e28c8c
ryanchankh <ryanchankh@berkeley.edu>
2019-03-22 16:17:12 -0700

    add params to save images

--- params/ica_subspace_params.py
+++ params/ica_subspace_params.py
@@ -12,20 +12,25 @@ class params(ica_params):
         self.version = "0.0"
         self.data_type = "vanHateren"
 
-        self.batch_size = 1
+        self.batch_size = 100
 
 
         self.num_groups = 16
         self.group_sizes = None
 
-        self.center_data = True
-        self.whiten_data = True
+        self.center_data = False
+        self.whiten_data = False
         self.lpf_data = False
-        self.whiten_method = "PCA"
+        self.whiten_method = "ZCA"
+
+        # model output intervals
+        self.gen_plot_int = 2e3
+        self.cp_int = 5e3
+        self.log_int = 100
 
         self.schedule = [
             {"weights": None, 
-             "num_batches": int(1e4),
+             "num_batches": int(1e2),
              "weight_lr": 0.001, 
              "decay_steps": 1,
              "decay_rate": 1.0,
