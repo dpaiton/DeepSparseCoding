c95c824b1e44a0d1aa192d38ef3a094733e8ab6d
ryanchankh <ryanchankh@berkeley.edu>
2019-04-16 16:45:59 -0700

    update batch size and learing rate

--- params/ica_subspace_params.py
+++ params/ica_subspace_params.py
@@ -7,20 +7,26 @@ import numpy as np
 class params(ica_params):
     def __init__(self):
         super(params, self).__init__()
+
+        # model config
         self.model_type = "ica_subspace"
         self.model_name = "ica_subspace"
         self.version = "0.0"
-        self.data_type = "vanHateren"
 
-        self.batch_size = 100
+        # data config
+        self.data_type = "mnist"
+        self.batch_size = 500
+        self.norm_data = False
+        
 
+        # hyperparams
+        self.num_groups = 64
+        self.group_sizes = [4 for _ in range(self.num_groups)]
 
-        self.num_groups = 16
-        self.group_sizes = None
-
-        self.center_data = False
-        self.whiten_data = False
-        self.lpf_data = False
+        # data preprocessing
+        self.center_data = True #False
+        self.whiten_data = True #False
+        self.lpf_data = True # False
         self.whiten_method = "ZCA"
 
         # model output intervals
@@ -28,10 +34,13 @@ class params(ica_params):
         self.cp_int = 5e3
         self.log_int = 100
 
+        # directories
+        self.display_dir = os.path.expanduser("~")+"/Work/Projects/"+self.model_name+"/vis/"
+
         self.schedule = [
             {"weights": None, 
-             "num_batches": int(1e2),
-             "weight_lr": 0.001, 
+             "num_batches": 2000,# int(1e4),
+             "weight_lr": float(0.01), 
              "decay_steps": 1,
              "decay_rate": 1.0,
              "staircase": False}
