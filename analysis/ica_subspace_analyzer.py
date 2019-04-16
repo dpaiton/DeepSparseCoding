905502165385793ad80670337791f9abc7e21522
ryanchankh <ryanchankh@berkeley.edu>
2019-04-16 16:44:36 -0700

    add some analysis tools

--- analysis/ica_subspace_analyzer.py
+++ analysis/ica_subspace_analyzer.py
@@ -1,3 +1,13 @@
-import os 
 import numpy as np 
 import tensorflow as tf
+from analysis.base_analyzer import Analyzer
+import utils.data_processing as dp 
+
+class IcaAnalyzer(Analyzer):
+    def __init__(self):
+        Analyzer.__init__(self)
+        self.var_names = [
+                "weights/w_synth:0"
+                ]
+
+
