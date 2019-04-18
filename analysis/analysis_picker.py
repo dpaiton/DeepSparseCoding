1396d309681831b953554d6f2ead1452afb421ee
Dylan <dpaiton@gmail.com>
2019-04-18 14:11:45 -0700

    adds mlp_ae to analysis_picker

--- analysis/analysis_picker.py
+++ analysis/analysis_picker.py
@@ -12,7 +12,7 @@ from analysis.vae_analyzer import VaeAnalyzer as vae_analyzer
 from analysis.gdn_autoencoder_analyzer import GaAnalyzer as ga_analyzer
 
 def get_analyzer(model_type):
-  if model_type.lower() in ["mlp", "mlp_vae", "mlp_lca", "mlp_sae", "mlp_lista"]:
+  if model_type.lower() in ["mlp", "mlp_ae", "mlp_vae", "mlp_lca", "mlp_sae", "mlp_lista"]:
     return mlp_analyzer()
   if model_type.lower() == "ica":
     return ica_analyzer()
