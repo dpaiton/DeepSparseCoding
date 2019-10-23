548bcc9c9bf3c3a18a3ef7d9087b396e4e5c9327
Dylan <dpaiton@gmail.com>
2019-10-23 08:10:22 -0700

    adds evals & basis analysis

--- analysis/mlp_analyzer.py
+++ analysis/mlp_analyzer.py
@@ -3,10 +3,14 @@ from analysis.base_analyzer import Analyzer
 class MlpAnalyzer(Analyzer):
   def __init__(self):
     super(MlpAnalyzer, self).__init__()
-    self.var_names = ["mlp/layer0/conv_w_0:0"]
+    self.var_names = ["mlp/layer0/fc_w_0:0"]
 
   def run_analysis(self, images, labels, save_info=""):
     super(MlpAnalyzer, self).run_analysis(images, labels, save_info=save_info)
+    if self.analysis_params.do_evals:
+      self.evals = self.eval_analysis(images, self.var_names, save_info)
+    if self.analysis_params.do_basis_analysis:
+      self.bf_stats = self.basis_analysis(self.evals["mlp/layer0/fc_w_0:0"], save_info)
     if self.analysis_params.do_class_adversaries:
       #TODO generate target labels based on options here
       #For now, defined in analysis_params
@@ -17,4 +21,5 @@ class MlpAnalyzer(Analyzer):
         target_labels = self.analysis_params.adversarial_target_labels,
         save_info=save_info)
     elif self.analysis_params.do_neuron_visualization:
+      print("Neuron Visualization Analysis")
       self.neuron_visualization_analysis(save_info=save_info)
