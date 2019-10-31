917347cb5db53cce8355e739fee81975f7c8e9a2
ryanchankh <ryanchankh@berkeley.edu>
2019-10-30 22:34:14 -0700

    fix bug in function

--- analysis/ica_subspace_analyzer.py
+++ analysis/ica_subspace_analyzer.py
@@ -12,7 +12,7 @@ class IcaSubspaceAnalyzer(Analyzer):
       "inference/latent_vars:0"]
 
   def run_analysis(self, images, labels=None, save_info=""):
-    super(IcaAnalyzer, self).run_analysis(images, labels, save_info=save_info)
+    super(IcaSubspaceAnalyzer, self).run_analysis(images, labels, save_info=save_info)
     if self.analysis_params.do_evals:
       self.evals = self.eval_analysis(images, self.var_names, save_info)
     if self.do_basis_analysis:
