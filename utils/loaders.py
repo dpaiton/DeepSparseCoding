0a773c2f71b2e4b8318dfbcf7a2e6ff7244a59f3
Dylan <dpaiton@gmail.com>
2020-06-17 15:44:48 +0200

    generalizes root_dir definition

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-ROOT_DIR = os.path.dirname(os.getcwd())
+ROOT_DIR = os.getcwd()
+while 'DeepSparseCoding' in ROOT_DIR:
+    ROOT_DIR = os.path.dirname(ROOT_DIR)
 if ROOT_DIR not in sys.path: sys.path.append(ROOT_DIR)
 
 import DeepSparseCoding.utils.file_utils as file_utils
