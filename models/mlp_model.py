11f0f326b658327b5f01cec7de1e0ca585545426
Dylan <dpaiton@gmail.com>
2020-06-23 00:35:53 +0200

    changed output to be logits instead of log softmax

--- models/mlp_model.py
+++ models/mlp_model.py
@@ -1,5 +1,7 @@
 import numpy as np
-import torch.nn.functional as F
+
+import torch
+#import torch.nn.functional as F
 
 from DeepSparseCoding.models.base_model import BaseModel
 from DeepSparseCoding.modules.mlp_module import MlpModule
@@ -13,7 +15,9 @@ class MlpModel(BaseModel, MlpModule):
     def get_total_loss(self, input_tuple):
         input_tensor, input_label = input_tuple
         pred = self.forward(input_tensor)
-        return F.nll_loss(pred, input_label)
+        #return F.nll_loss(pred, input_label)
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+        return self.loss_fn(pred, input_label)
 
     def generate_update_dict(self, input_data, input_labels=None, batch_step=0, update_dict=None):
         if update_dict is None:
@@ -24,7 +28,8 @@ class MlpModel(BaseModel, MlpModule):
             'batch_step':batch_step,
             'train_progress':np.round(batch_step/self.params.num_batches, 3)}
         pred = self.forward(input_data)
-        total_loss = F.nll_loss(pred, input_labels)
+        #total_loss = F.nll_loss(pred, input_labels)
+        total_loss = self.loss_fn(pred, input_labels)
         pred = pred.max(1, keepdim=True)[1]
         correct = pred.eq(input_labels.view_as(pred)).sum().item()
         stat_dict['loss'] = total_loss.item()
